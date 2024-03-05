ed490c4693b4a9fbe2bb486781f321492a0fb269
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-03-05 10:17:05 +0500

    Update seccyc.py

--- seccyc.py
+++ seccyc.py
@@ -1,7 +1,8 @@
 def calculate_a(k):
     a, b = 1, 1
     for _ in range(k-1):
-        a, b = 0.5 * (b ** 0.5 + 0.5 * a ** 0.5), a
+        a = 0.5 * (b ** 0.5 + 0.5 * a ** 0.5)
+        b = a 
     return a
 
-print(calculate_a(2))
+print(calculate_a(5))
