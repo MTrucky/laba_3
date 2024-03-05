f950713c36732a3da6c69d58959dc784ac04a168
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-03-05 10:08:34 +0500

    Update secrec.py

--- secrec.py
+++ secrec.py
@@ -2,8 +2,8 @@ def calculate_a(k, a=1, b=1):
     if k ==  1:
         return a
     else:
-        a_prev = calculate_a(k -  1, a, b)
-        b_prev = calculate_a(k -  1, a, b)
+        a_prev = calculate_a(k-1, a, b)
+        b_prev = calculate_a(k-1, a, b)
         
         a_k =  0.5 * (b_prev **  0.5 +  0.5 * a_prev **  0.5)
         
